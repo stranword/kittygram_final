9e019608ead2eacd61c13930bf76eca1da4df074
Михаил Чернавин <chemm96@yandex.ru>
2024-04-22 13:08:58 +0300

    add enrtypoint, add secrets in env, separated pytest and linters

---
+++ backend/entrypoint.sh
@@ -0,0 +1,3 @@
+sudo docker compose -f docker-compose.production.yml exec backend python manage.py migrate
+sudo docker compose -f docker-compose.production.yml exec backend python manage.py collectstatic
+sudo docker compose -f docker-compose.production.yml exec backend cp -r /app/collected_static/. /backend_static/static/
