38c1ce7c4468c4d9add3493ff26dd689858a858f
Михаил Чернавин <chemm96@yandex.ru>
2024-04-22 14:40:56 +0300

    add entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,3 +1,5 @@
+gunicorn --bind 0.0.0.0:9000 kittygram_backend.wsgi
+sleep 10
 sudo docker compose -f docker-compose.production.yml exec backend python manage.py migrate
 sudo docker compose -f docker-compose.production.yml exec backend python manage.py collectstatic
 sudo docker compose -f docker-compose.production.yml exec backend cp -r /app/collected_static/. /backend_static/static/
