be05618de39670be4ac0a05ac0233bf00e20e6b6
Михаил Чернавин <chemm96@yandex.ru>
2024-04-25 18:33:13 +0300

    add entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+gunicorn kittygram.wsgi:application --bind 0.0.0.0:8000
 python manage.py migrate
 python manage.py collectstatic
-gunicorn kittygram.wsgi:application --bind 0.0.0.0:8000
+
