5f5fd5ae742b67e3f61ebad4c635a332a89629d2
Михаил Чернавин <chemm96@yandex.ru>
2024-04-26 17:49:03 +0300

    change entrypoint, add prune

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 gunicorn kittygram.wsgi:application --bind 0.0.0.0:8000
+sleep 10
 python manage.py migrate
 python manage.py collectstatic
 python manage.py cp -r /app/collected_static/. /backend_static/static/
