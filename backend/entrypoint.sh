62ff2e05a3fe0cad347c503f8ce47ca2ea74f2d7
Михаил Чернавин <chemm96@yandex.ru>
2024-05-14 22:25:34 +0300

    fix entrypoint and dockerfile backend

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,5 +4,4 @@ gunicorn kittygram.wsgi:application --bind 0.0.0.0:8000
 sleep 10
 python manage.py migrate
 python manage.py collectstatic
-python manage.py cp -r /app/collected_static/. /backend_static/static/
-echo 'yes'
+mv /app/collected_static/* /backend_static/static/
