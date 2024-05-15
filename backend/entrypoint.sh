538da64f5a2f29d86fed6e4e727ef8e27fd5d011
Михаил Чернавин <chemm96@yandex.ru>
2024-05-15 11:21:09 +0300

    chnge copy static in entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,6 +4,6 @@
 
 /usr/local/bin/python /app/manage.py collectstatic --noinput
 
-/usr/local/bin/python /app/manage.py cp -r /app/collected_static/. /backend_static/static/
+mv /app/collected_static/* /backend_static/static/
 
 gunicorn kittygram_backend.wsgi:application --bind 0.0.0.0:8000
