4461f142efab26e1ab182dfc4f09ae821b199dbd
Михаил Чернавин <chemm96@yandex.ru>
2024-08-01 12:57:52 +0300

    change entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-python /app/manage.py migrate
+python manage.py migrate
 
-python /app/manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
-mv /app/collected_static/* /backend_static/static/
+cp -r /app/collected_static/. /backend_static/static/
 
-gunicorn kittygram_backend.wsgi:application --bind 0.0.0.0:9000
+exec "$@"
